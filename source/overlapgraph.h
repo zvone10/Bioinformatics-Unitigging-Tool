25f7823ba2393e6f2c25897c75c9f45a929aea66
Petar Kovacevic <simunic.petar@gmail.com>
2015-01-10 23:08:07 +0100

    expanded interface with private members and methods required to do unique join collapsing algorithm

--- source/overlapgraph.h
+++ source/overlapgraph.h
@@ -25,10 +25,13 @@ class OverlapGraph
 		std::vector< std::vector<overlap> > graph;
 		std::vector< std::vector<overlap> > reducedGraph;
 		std::vector< Chunk > collapsedReducedGraph;
+		std::vector<int> reducedGraphVertexReferenceCounter;
 		void initialize();
 		void containedReadRemoval();
 		void transitiveEdgeRemoval();
 		void uniqueJoinCollapsing();
+		void calculateChunks(int chunkStartingVertexId, int requestedChunkId);
+		bool vertexAlreadyInChunk(int vertexId);
 	public:
 		OverlapGraph(std::vector<std::string> reads, std::vector<overlap> overlaps);
 		void runUnitigging();
